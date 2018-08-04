566750a9a4e15d432c8165988c43771d65e80dca
Marco Marchitelli <marchitelli97@libero.it>
2018-08-04 20:17:52 +0200

    Aggiunto main menu con omino bellino penzolino.

---
+++ Assets/Script/Animations/OminoBellino.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class OminoBellino : MonoBehaviour {
+
+    public Animator animator;
+    public float animSpeedUpDuration = 1.5f;
+
+    float timer = 0;
+    bool isTouched = false;
+
+    public void OnOminoTouched()
+    {
+        print("Toccato l'omino");
+        if (animator && !isTouched)
+        {
+            animator.SetTrigger("SpeedUp");
+            isTouched = true;
+            StartCoroutine(TimeCounter());
+        }
+    }
+
+    IEnumerator TimeCounter()
+    {
+        print("Partita speedUp");
+        while (timer < animSpeedUpDuration)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        animator.SetTrigger("SpeedDown");
+        isTouched = false;
+    }
+}
--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
@@ -9,5 +7,4 @@ public class MainMenu : MonoBehaviour {
     {
         SceneManager.LoadScene(sceneindex);
     }
-
 }
