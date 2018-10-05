26b7e7cf1a8e8f40286ad70d3d5e6857931bc264
Marco Marchitelli <marchitelli97@libero.it>
2018-10-05 15:18:57 +0200

    Credits scorrono :)

---
+++ Assets/CreditsController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CreditsController : MonoBehaviour {
+
+	public UnityEvent EscPress;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape))
+			EscPress.Invoke ();
+	}
+}
